b85fa9fb4aa801469a847ef77e08c2092e116c4a
Sergio Santos <slns@portugalmail.pt>
2020-03-12 09:33:52 +0000

    Change Update tasks, only update if done is false, and dont update if parametrs not allow

--- src/controllers/tasks/update.js
+++ src/controllers/tasks/update.js
@@ -1,87 +1,83 @@
 'use strict';
 
-const { TasksModel } = require('../../models');
-const { UpdateValidator } = require('../../validators/tasks');
+const {
+	TasksModel
+} = require('../../models');
+const {
+	UpdateValidator
+} = require('../../validators/tasks');
 
 function update(taskId, task) {
-    try {
-        return TasksModel.findOneAndUpdate({
-            _id: taskId
-        }, task,
-        {
-            returnOriginal: false
-        });
-    } catch (error) {
-        return error;
-    }    
+	try {
+		return TasksModel.findOneAndUpdate({
+			_id: taskId
+		}, task, {
+			returnOriginal: false
+		});
+	} catch (error) {
+		return error;
+	}
 }
 
 function select(taskId) {
-    try {
-      return TasksModel.findById(taskId);
-      
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		return TasksModel.findById(taskId);
+
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 module.exports = (request, response) => {
-    const { taskId } = request.params;
-    const { body: task } = request;
+	const {
+		taskId
+	} = request.params;
+	const {
+		body: task
+	} = request;
 
-    if (!taskId) {
-        return response.status(500).json({
-            status: false,
-            message: 'Task Id is required'
-        });
-    }
-    
-    let taskResult;
-    select(taskId)
-        .then((task) => {
-            console.log('tasksObject ', task);
-            taskResult = task;
-           return task;
-        })
-        .catch((error) => {
-            return response.status(500).json({
-                status: false,
-                message: error.message
-            });
-        });
+	if (!taskId) {
+		return response.status(500).json({
+			status: false,
+			message: 'Task Id is required'
+		});
+	}
 
-
-    UpdateValidator.validateAsync(task)
-        .then(() => {
-            return update(taskId, task)
-                .then(task => {
-                        return response.status(200).json({
-                            status: true,
-                            message: task
-                        });
-                })
-                .catch((error) => {
-                    return response.status(500).json({
-                        status: false,
-                        message: error.message
-                    });
-                });
-            })
-        .catch((error) => {
-            return response.status(500).json({
-                status: false,
-                message: error.message
-                })
-            });
-
-
-   // const taskObject = taskById(taskId);
-  //  console.log('taskObject ', taskObject);
-
-  //  if (taskObject.done) {;
-   //     return response.status(412).json({
-   //         status: false,
-    //        message: 'It is only allowed to change unfinished tasks';
-    //    });
-   // };
+	UpdateValidator.validateAsync(task)
+		.then(() => {
+			return select(taskId)
+		.then((taskSelect) => {
+			if (taskSelect.done) {
+				return response.status(412).json({
+					status: false,
+					message: 'It is not allowed to change completed tasks'
+				});
+			}
+			return update(taskId, task)
+		.then((task) => {
+				return response.status(200).json({
+					status: true,
+					message: task
+				});
+			})
+		.catch((error) => {
+			return response.status(500).json({
+				status: false,
+				message: error.message
+			});
+		});
+		})
+		.catch((error) => {
+			return response.status(500).json({
+				status: false,
+				message: error.message
+			});
+		});
+		})
+		.catch((error) => {
+			return response.status(500).json({
+				status: false,
+				message: error.message
+			})
+		});
 };
